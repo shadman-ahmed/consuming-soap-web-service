fd226bdf51a2f455907cca9fff6f22ad561886cf
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2020-03-21 12:51:08 +0600

    initial commit

---
+++ src/main/java/com/shadman_ahmed/consumesoapws/ConsumeSoapWsApplication.java
@@ -0,0 +1,13 @@
+package com.shadman_ahmed.consumesoapws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeSoapWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeSoapWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shadman_ahmed/consumesoapws/ConsumeSoapWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadman_ahmed.consumesoapws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumeSoapWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
