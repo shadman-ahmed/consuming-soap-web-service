a06ed4d7439c22fb4c878e3b023e8e6a6a96e476
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2020-03-21 14:29:31 +0600

    Remove: unused imports

--- src/main/java/com/shadman_ahmed/consumesoapws/controller/CountryController.java
+++ src/main/java/com/shadman_ahmed/consumesoapws/controller/CountryController.java
@@ -5,11 +5,9 @@ import com.example.consumingwebservice.wsdl.GetCountryResponse;
 import com.shadman_ahmed.consumesoapws.client.CountryClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.Objects;
 
